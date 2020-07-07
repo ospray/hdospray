0513410eb56e89a9923f0e6271a2321a71565574
Sebastian Herholz <sebastian.herholz@intel.com>
2020-07-07 16:40:28 +0200

    [lights] added support for setting the number of light samples used for NEE

--- pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
+++ pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
@@ -183,6 +183,9 @@ HdOSPRayRenderDelegate::_Initialize()
              HdOSPRayRenderSettingsTokens->samplesToConvergence,
              VtValue(
                     int(HdOSPRayConfig::GetInstance().samplesToConvergence)) });
+     _settingDescriptors.push_back(
+           { "lightSamples", HdOSPRayRenderSettingsTokens->lightSamples,
+             VtValue(int(HdOSPRayConfig::GetInstance().lightSamples)) });
     _settingDescriptors.push_back(
            { "lightSamples", HdOSPRayRenderSettingsTokens->lightSamples,
              VtValue(int(HdOSPRayConfig::GetInstance().lightSamples)) });
