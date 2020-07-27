ffca570ff8b278b2cc00344c7147fef83bfb3f56
Sebastian Herholz <sebastian.herholz@intel.com>
2020-07-27 14:04:16 +0200

    moved ospInit and ospShutdown from the RenderDelegate to the RenderPlugin
    The initialization and shutdown had to be moved since calling ospshutdown in
    the destructor of HDOSPRayRenderDelegate frees the device before all OSP classes are
    destroyed (e.g., _renderer)

--- pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
+++ pxr/imaging/plugin/hdOSPRay/renderDelegate.cpp
@@ -90,59 +90,15 @@ HdOSPRayRenderDelegate::HdOSPRayRenderDelegate(
 void
 HdOSPRayRenderDelegate::_Initialize()
 {
-    // Check plugin against pxr version
-#if PXR_MAJOR_VERSION != 0 || PXR_MINOR_VERSION != 20
-    error This version of HdOSPRay is configured to built against USD v0 .20.x
-#endif
-
-           int ac
-           = 1;
-    std::string initArgs = HdOSPRayConfig::GetInstance().initArgs;
-    std::stringstream ss(initArgs);
-    std::string arg;
-    std::vector<std::string> args;
-    while (ss >> arg) {
-        args.push_back(arg);
-    }
-    ac = static_cast<int>(args.size() + 1);
-    const char** av = new const char*[ac];
-    av[0] = "ospray";
-    for (int i = 1; i < ac; i++) {
-        av[i] = args[i - 1].c_str();
-    }
-    int init_error = ospInit(&ac, av);
-    if (init_error != OSP_NO_ERROR) {
-        std::cerr << "FATAL ERROR DURING INITIALIZATION!" << std::endl;
-    } else {
-        auto device = ospGetCurrentDevice();
-        if (device == nullptr) {
-            std::cerr << "FATAL ERROR DURING GETTING CURRENT DEVICE!"
-                      << std::endl;
-        }
-
-        ospDeviceSetStatusFunc(device,
-                               [](const char* msg) { std::cout << msg; });
-        ospDeviceSetErrorFunc(device, [](OSPError e, const char* msg) {
-            std::cerr << "OSPRAY ERROR [" << e << "]: " << msg << std::endl;
-        });
-
-        ospDeviceCommit(device);
-    }
-    if (ospGetCurrentDevice() == nullptr) {
-        // user most likely specified bad arguments, retry without them
-        ac = 1;
-        ospInit(&ac, av);
-    }
-    delete[] av;
 
 #ifdef HDOSPRAY_PLUGIN_PTEX
     ospLoadModule("ptex");
 #endif
 
     if (HdOSPRayConfig::GetInstance().usePathTracing == 1)
-        _renderer = ospNewRenderer("pathtracer");
+        _renderer = opp::Renderer("pathtracer");
     else
-        _renderer = ospNewRenderer("scivis");
+        _renderer = opp::Renderer("scivis");
 
     // Store top-level OSPRay objects inside a render param that can be
     // passed to prims during Sync().
@@ -228,8 +184,6 @@ HdOSPRayRenderDelegate::~HdOSPRayRenderDelegate()
     }
 
     _renderParam.reset();
-
-    ospShutdown();
 }
 
 HdRenderParam*
