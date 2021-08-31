3e5732a201ea90917e5d45001d2a794c01fe9e3e
Sebastian Herholz <sebastian.herholz@intel.com>
2021-08-31 14:49:37 +0200

    Switched to OSPRay v2.7 which fixes the previous OIDN build problem

--- scripts/superbuild/cmake/ep_ospray.cmake
+++ scripts/superbuild/cmake/ep_ospray.cmake
@@ -36,7 +36,6 @@ set(ARGS_OSPRAY_DOWNLOAD URL ${SUPERBUILD_OSPRAY_URL})
       # -Drkcommon_DIR=${CMAKE_INSTALL_PREFIX}/lib/cmake/rkcommon-1.6.1
       # -Dopenvkl_DIR=${CMAKE_INSTALL_PREFIX}/lib/cmake/openvkl-0.13.0
     INSTALL_COMMAND ""
-    PATCH_COMMAND patch ${CMAKE_BINARY_DIR}/${EP_OSPRAY}/source/scripts/superbuild/dependencies/dep_oidn.cmake < ${CMAKE_SOURCE_DIR}/patches/OSPRay_superbuild_OIDN.patch COMMAND patch ${CMAKE_BINARY_DIR}/${EP_OSPRAY}/source/scripts/superbuild/CMakeLists.txt < ${CMAKE_SOURCE_DIR}/patches/OSPRay_superbuild_CMakeLists.patch
   )
 
   ExternalProject_Add_StepDependencies(${EP_OSPRAY}
